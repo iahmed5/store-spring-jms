cace9dc14d85eaa42c630d02a3a6525914174f64
iahmed5 <example.com>
2021-07-25 15:07:08 +0100

    Initial commit

---
+++ store-spring-jms/src/main/java/com/company/storespringjms/StoreSpringJmsApplication.java
@@ -0,0 +1,13 @@
+package com.company.storespringjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSpringJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSpringJmsApplication.class, args);
+	}
+
+}
