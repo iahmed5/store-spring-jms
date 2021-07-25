0046cb4c1490171c799a66e6dd223b45730d32ea
iahmed5 <example.com>
2021-07-25 23:00:00 +0100

    jms-spring-default-factory working as subproject

---
+++ jms-spring-default-factory/src/main/java/com/company/storespringjms/SpringDefaultFactoryApplication.java
@@ -0,0 +1,34 @@
+package com.company.storespringjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+
+@SpringBootApplication
+public class SpringDefaultFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDefaultFactoryApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(
+				SpringDefaultFactoryApplication.class, args
+		);
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		jmsTemplate.convertAndSend("order-queue", "Hello World");
+	}
+
+	@Bean
+	public JmsListenerContainerFactory warehouseFactory(ConnectionFactory factory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory containerFactory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(containerFactory, factory);
+		return containerFactory;
+	}
+
+}
